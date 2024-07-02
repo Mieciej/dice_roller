4b9b082af775d942bee5eb2e9c98e4d78904c658
Maciej Janicki <maciej.ja@proton.me>
2024-07-02 17:18:09 +0200

    Cleanup, main.c

--- main.c
+++ main.c
@@ -8,24 +8,30 @@ void fail(const char *message) {
   exit(EXIT_FAILURE);
 }
 
+unsigned int token_roll(char *token) {
+
+  char *d = strchr(token, 'd');
+  if (d == NULL) {
+    fail("Incorrect roll syntax. Use \'d\' to specify number of dice sides");
+  }
+  size_t end = strlen(token);
+  if (token[end - 1] == 'd') {
+    fail("Incorrect roll syntax");
+  }
+  *d = '\0';
+  long long nRolls = atol(token);
+  nRolls = nRolls > 0 ? nRolls : 1;
+  long long nSides = atol(d + 1);
+  nSides = nSides > 0 ? nSides : 1;
+  *d = 'd';
+  return roll_dice(NULL, nRolls, nSides);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fail("Too few arguments");
   }
-  char *d = strchr(argv[1], 'd');
-  if (d == NULL) {
-    fail("Incorrect roll syntax. Use \'d\' to specify number of dice sides");
-  }
-  size_t end = strlen(argv[1]);
-  if (argv[1][end - 1] == 'd') {
-    fail("Incorrect roll syntax");
-  }
-  *d = '\0';
-  long long nRolls = atol(argv[1]);
-  nRolls = nRolls > 0 ? nRolls : 1;
-  long long nSides = atol(d + 1);
-  nSides = nSides > 0 ? nSides : 1;
-  unsigned int result = roll_dice(NULL, nRolls, nSides);
+  unsigned int result = token_roll(argv[1]);
   printf("%u\n", result);
   return EXIT_SUCCESS;
 }
